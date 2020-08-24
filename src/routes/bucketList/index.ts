4b3d2adfa551166a20a7889a4306db06fbe3a91c
sihwang <gommpo111@gmail.com>
2020-08-24 14:30:29 +0900

    버킷리스트 리스트 및 상세쪽 마지막 변경 기간 조회(캐싱 최적화 용)

--- src/routes/bucketList/index.ts
+++ src/routes/bucketList/index.ts
@@ -1,6 +1,11 @@
 import Router from '@koa/router';
 import {resError, resOK} from "../../utils/common";
-import {getBucketList, getBucketListById} from "../../service/bucketListService";
+import {
+    getBucketList,
+    getBucketListById,
+    getBucketListLastUpdatedDate,
+    isBucketListUpdate
+} from "../../service/bucketListService";
 
 const router = new Router();
 const userId = 1;
@@ -8,6 +13,11 @@ const userId = 1;
 router.get('/', async (ctx) => {
     const bucketList = await getBucketList(userId);
     return resOK(ctx, bucketList ? bucketList : []);
+});
+
+router.get('/last-update-date', async (ctx) => {
+    const lastUpdateDate = await getBucketListLastUpdatedDate(userId);
+    return resOK(ctx, lastUpdateDate);
 });
 
 router.get('/:bucketListId', async (ctx) => {
@@ -18,6 +28,18 @@ router.get('/:bucketListId', async (ctx) => {
     }
 
     const result = await getBucketListById(Number(bucketListId), userId);
+    return resOK(ctx, result);
+});
+
+router.get('/:bucketListId/last-update', async (ctx) => {
+    const { lastUpdatedAt } = ctx.query;
+    const { bucketListId } = ctx.params;
+
+    if (!lastUpdatedAt || !bucketListId || !Number.isInteger(Number(bucketListId))) {
+        return resError({ctx, errorCode: 400, message:  `lastUpdatedAt: ${lastUpdatedAt}, bucketListId: ${bucketListId} is not allow request param`});
+    }
+
+    const result = await isBucketListUpdate({id: Number(bucketListId), lastUpdatedAt, userId});
     return resOK(ctx, result);
 });
 
--- src/service/bucketListService.ts
+++ src/service/bucketListService.ts
@@ -1,10 +1,38 @@
 import {BucketList} from "../entity/BucketList";
+import {Account} from "../entity/Account";
+import CommonError from "../error/CommonError";
+import {getAccountByIdAndUserId} from "./accountService";
 
 export const getBucketList = async (userId: number) => {
     return await BucketList.find({relations:['todoList'], where: {userId}});
 };
 
+export const getBucketListLastUpdatedDate = async (userId: number) => {
+    const bucketList = await BucketList.findOne({ order: {updatedAt: 'DESC'}, where: { userId } });
+
+    // 없으면 캐시할 내용이 없는거니 현재일시 전달
+    if(!bucketList) {
+        return new Date();
+    }
+
+    return bucketList.updatedAt;
+};
+
 export const getBucketListById = async (id: number ,userId: number) => {
     return await BucketList.findOne({relations:['todoList'], where: {id, userId}});
+};
+
+export const isBucketListUpdate = async ({ id, userId, lastUpdatedAt }: {
+    id: number;
+    userId: number;
+    lastUpdatedAt: Date;
+}) => {
+    const bucketList = await getBucketListById(id, userId);
+
+    if (!bucketList) {
+        throw new CommonError(`accountId:${id} is not found`, 400);
+    }
+
+    return bucketList.updatedAt.getTime() === new Date(lastUpdatedAt).getTime();
 };
 
