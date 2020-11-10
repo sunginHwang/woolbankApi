63e4a4de30f50a89ddae8d33c302129107eb2782
sihwang <gommpo111@gmail.com>
2020-11-10 14:52:43 +0900

    env 세팅

--- ecoConfig/process.prod.config.js
+++ ecoConfig/process.prod.config.js
@@ -24,7 +24,11 @@ module.exports = {
         DEBUG: 'test:banket-list:*',
         DEBUG_COLORS: true
       },
-      instances: 2,
+      max_memory_restart: 50,
+      node_args: [
+        '--max_old_space_size=50'
+      ],
+      instances: 1,
       // min uptime of the app to be considered started
       min_uptime: 5000,
       // number of consecutive unstable restarts (less than 1sec interval or custom time via min_uptime) before your app is considered errored and stop being restarted
