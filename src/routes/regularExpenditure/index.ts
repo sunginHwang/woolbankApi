ec4fb1000d33bf6e467ce7aeb0ab20afff6cec34
sihwang <gommpo111@gmail.com>
2021-02-27 17:53:42 +0900

    정기지출 리스트 정렬 조건 추가

--- src/routes/regularExpenditure/index.ts
+++ src/routes/regularExpenditure/index.ts
@@ -1,53 +1,65 @@
-import Router from "@koa/router";
+import Router from '@koa/router';
 
-import isAuthenticated from "../../middleware/isAuthenticated";
+import isAuthenticated from '../../middleware/isAuthenticated';
 import {
-    getRegularExpenditureListByUserId,
-    removeRegularExpenditure,
-    saveRegularExpenditure
-} from "../../service/regularExpenditureService";
-import {resError, resOK} from "../../utils/common";
-import {SaveRegularExpenditureReqType} from "../../models/routes/SaveRegularExpenditureReqType";
+  getExpenditureTypeList,
+  getRegularExpenditureListByUserId,
+  removeRegularExpenditure,
+  saveRegularExpenditure,
+  getRegularExpenditureWithType
+} from '../../service/regularExpenditureService';
+import { resError, resOK } from '../../utils/common';
+import { SaveRegularExpenditureReqType } from '../../models/routes/SaveRegularExpenditureReqType';
 
 const router = new Router();
 
-
 router.get('/', isAuthenticated, async (ctx) => {
-    const { limit } = ctx.query;
-    const regularExpenditures = await getRegularExpenditureListByUserId(ctx.userId, limit);
+  const { limit } = ctx.query;
+  const [regularExpenditures, expenditureTypeList] = await Promise.all([
+    getRegularExpenditureListByUserId(ctx.userId, limit),
+    getExpenditureTypeList()
+  ]);
 
-    return resOK(ctx, regularExpenditures ? regularExpenditures : []);
+  const response = expenditureTypeList
+    .map((expenditureType) => getRegularExpenditureWithType(expenditureType, regularExpenditures))
+    .filter((a) => a.list.length > 0);
+
+  return resOK(ctx, response ? response : []);
 });
 
-
 router.post('/', isAuthenticated, async (ctx) => {
-    const reqType: SaveRegularExpenditureReqType = ctx.request.body;
-    const isNowAllowValidation = reqType?.title === '' || reqType?.amount < 0 || reqType?.regularDate < 0 || reqType?.expenditureTypeId < 0;
+  const reqType: SaveRegularExpenditureReqType = ctx.request.body;
+  const isNowAllowValidation =
+    reqType?.title === '' || reqType?.amount < 0 || reqType?.regularDate < 0 || reqType?.expenditureTypeId < 0;
 
-    if (isNowAllowValidation) {
-        return resError({ ctx, errorCode: 400, message: 'body validation fail' });
-    }
+  if (isNowAllowValidation) {
+    return resError({ ctx, errorCode: 400, message: 'body validation fail' });
+  }
 
-    const regularExpenditure = await saveRegularExpenditure(reqType, ctx.userId);
+  const regularExpenditure = await saveRegularExpenditure(reqType, ctx.userId);
 
-    if (!regularExpenditure) {
-        return resError({ ctx, errorCode: 500, message: 'not saved regularExpenditure' });
-    }
+  if (!regularExpenditure) {
+    return resError({ ctx, errorCode: 500, message: 'not saved regularExpenditure' });
+  }
 
-    resOK(ctx, {
-        regularExpenditureId: regularExpenditure.id
-    });
+  resOK(ctx, {
+    regularExpenditureId: regularExpenditure.id
+  });
 });
 
 router.delete('/:regularExpenditureId', isAuthenticated, async (ctx) => {
-    const { regularExpenditureId } = ctx.params;
+  const { regularExpenditureId } = ctx.params;
 
-    if (!Number.isInteger(Number(regularExpenditureId))) {
-        return resError({ ctx, errorCode: 400, message: `regularExpenditureId: ${regularExpenditureId} is not allow request param` });
-    }
+  if (!Number.isInteger(Number(regularExpenditureId))) {
+    return resError({
+      ctx,
+      errorCode: 400,
+      message: `regularExpenditureId: ${regularExpenditureId} is not allow request param`
+    });
+  }
 
-    const result = await removeRegularExpenditure(regularExpenditureId);
-    resOK(ctx, result);
+  const result = await removeRegularExpenditure(regularExpenditureId);
+  resOK(ctx, result);
 });
 
 export default router;
--- src/service/regularExpenditureService.ts
+++ src/service/regularExpenditureService.ts
@@ -2,15 +2,43 @@ import {RegularExpenditure} from "../entity/RegularExpenditure";
 import {getConnection} from "typeorm";
 import {SaveRegularExpenditureReqType} from "../models/routes/SaveRegularExpenditureReqType";
 import CommonError from "../error/CommonError";
+import {ExpenditureType} from "../entity/ExpenditureType";
+import {getNowDate, getRemainDate} from "../utils/date";
 
 
 export const getRegularExpenditureListByUserId = async (userId: number, limit: number = 100) => {
     return await RegularExpenditure.find({
-        relations: ['expenditureType'],
         where: { userId },
         order: { id: 'DESC' },
         take: limit
     });
+}
+
+export const getExpenditureTypeList = async () => {
+    return await ExpenditureType.find({ order: { id: 'DESC' } });
+}
+
+export const getRegularExpenditureWithType = (expenditureType: ExpenditureType, regularExpenditures: RegularExpenditure[]) => {
+    const { id, type, name } = expenditureType;
+    const nowDate = getNowDate();
+
+    const regularExpenditureList = regularExpenditures
+        .filter((re) => re.expenditureTypeId === id)
+        .map((r) => Object.assign(r, { regularExpenditureDay: getRemainDate(r.regularDate) }));
+
+    // 정기지출 당일 리스트
+    const regularDateList = regularExpenditureList.filter((item) => item.regularDate === nowDate);
+    // 지출일까지 날짜 남은 리스트 -> 날짜 순 정렬
+    const notRegularDateList = regularExpenditureList
+        .filter((item) => item.regularDate !== nowDate)
+        .sort((a, b) => {
+            // 지출일 아닌건 날짜 정렬
+            return a.regularExpenditureDay.getTime() - b.regularExpenditureDay.getTime();
+        });
+
+    // 리스트 순서는 당일, 해당 달에 남은날, 다음달 이체일 순서
+    const list = [...regularDateList, ...notRegularDateList];
+    return Object.assign({ type, name }, { list });
 }
 
 export const saveRegularExpenditure = async (saveReq: SaveRegularExpenditureReqType, userId: number) => {
