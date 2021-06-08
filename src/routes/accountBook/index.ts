015117bcec7caa4a00a298fc7afa9e88dc24fda6
sihwang <gommpo111@gmail.com>
2021-06-08 22:04:43 +0900

    list api 스펙 연결

--- src/routes/accountBook/index.ts
+++ src/routes/accountBook/index.ts
@@ -1,34 +1,55 @@
-
 import Router from '@koa/router';
-import {
-    getAccountBooksByUserId, saveAccountBook
-} from '../../service/accountBookService';
+import { getAccountBooksByUserId, saveAccountBook } from '../../service/accountBookService';
 import { resError, resOK } from '../../utils/common';
-import isAuthenticated from "../../middleware/isAuthenticated";
-import {SaveAccountBookReqType} from "../../models/routes/SaveAccountBookReqType";
+import isAuthenticated from '../../middleware/isAuthenticated';
+import { SaveAccountBookReqType } from '../../models/routes/SaveAccountBookReqType';
+import { AccountBookCategoryType } from '../../models/AccountBookCategoryType';
 
 const router = new Router();
 
-router.get('/', isAuthenticated, async (ctx) => {
-    const { limit } = ctx.query;
+interface IListRes {
+  id: number;
+  title: string;
+  categoryName: string;
+  type: AccountBookCategoryType;
+  isRegularExpenditure: boolean;
+  amount: number;
+  registerDateTime: Date;
+}
 
-    const accountBooks = await getAccountBooksByUserId(ctx.userId, limit);
-    return resOK(ctx, accountBooks ? accountBooks : []);
+router.get('/', isAuthenticated, async (ctx) => {
+  const { limit } = ctx.query;
+
+  const accountBooks = await getAccountBooksByUserId(ctx.userId, limit);
+
+  const response: IListRes[] = accountBooks.map((accountBook) => {
+    const { id, title, type, isRegularExpenditure, amount, registerDateTime, accountBookCategory } = accountBook;
+    return {
+      id,
+      title,
+      type,
+      isRegularExpenditure,
+      amount,
+      registerDateTime,
+      categoryName: accountBookCategory.name
+    };
+  });
+  return resOK(ctx, response || []);
 });
 
 router.post('/', isAuthenticated, async (ctx) => {
-    const reqType: SaveAccountBookReqType = ctx.request.body;
+  const reqType: SaveAccountBookReqType = ctx.request.body;
 
-    const isValidRequest = !reqType.amount || !reqType.title || !reqType.registerDateTime || !reqType.categoryId || !reqType.type;
+  const isValidRequest =
+    !reqType.amount || !reqType.title || !reqType.registerDateTime || !reqType.categoryId || !reqType.type;
 
-    if (isValidRequest) {
-        return resError({ ctx, errorCode: 400, message: 'body validation fail' });
-    }
+  if (isValidRequest) {
+    return resError({ ctx, errorCode: 400, message: 'body validation fail' });
+  }
 
-    const accountBook = await saveAccountBook(reqType, ctx.userId);
+  const accountBook = await saveAccountBook(reqType, ctx.userId);
 
-    resOK(ctx, accountBook);
+  resOK(ctx, accountBook);
 });
-
 
 export default router;
